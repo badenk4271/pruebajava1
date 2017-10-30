e8ef3f3e316c004794b3285c114c348edf7e5a43
badenk4271 <ayavarav@gmail.com>
2017-10-30 19:56:39 -0300

    Resultado del area del rectangulo

--- src/pruebajava1/AreaRectangulo.java
+++ src/pruebajava1/AreaRectangulo.java
@@ -4,11 +4,40 @@
  * and open the template in the editor.
  */
 package pruebajava1;
-
-/**
- *
+import java.util.Scanner;
+/******************************************************************************
+ *Alejandro yavara Vargas
+ * 
+ * Fecha: octubre  octubre 30 de 2017
+ * Programa:AreaRectangulo
+ * Responsabilidad: Clase Principal
  * @author LAB01
- */
-public class AreaRectangulo {
-    
+ ******************************************************************************/
+public class AreaRectangulo 
+{
+ public static void main(String[] args)
+ {
+  int area;
+  
+  Scanner escaner=new Scanner(System.in);
+System.out.println("Ingrese la base del rectangulo:");
+int base = escaner.nextInt();
+
+System.out.println("Ingrese altura del rectangulo:");
+int altura = escaner.nextInt();
+area=base*altura;
+System.out.println("El area  del rectangulo es:"+area+" mts2");
+
+
+
+
+
+
+  
+
+  
+
+     
+ } 
+         
 }
